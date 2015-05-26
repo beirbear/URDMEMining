a40c4f79e729ae81db2da2bc719b822b720a8b70
beirbear <beir.bear@gmail.com>
2015-05-26 13:42:38 +0000

    Include output expoert to file

--- urdmlMiningPartialWindows.py
+++ urdmlMiningPartialWindows.py
@@ -71,21 +71,33 @@ def filterResult(input):
 
     return resultSet
 
+def convertFrozenSetToJson(input):
+    dataSingleObject = "["
+    for item in input:
+        resultKeys = item.keys()
+        resultValues = item.values()
+        resultData = "["
+        for i in range(len(resultKeys)):
+            resultData += "{ \"_key\" : \"" +  str(list(resultKeys[i])) + "\", \"_value\": " + str(resultValues[i]) + "}"
+            if i < len(resultKeys) - 1:
+                resultData += ","
+        dataSingleObject += resultData + "],"
+    return dataSingleObject[:-1] + "]"
+
 if __name__ == "__main__":
     if len(sys.argv) != 5:
         print("Usage: urdmeMiningPartialWindow <winSize> <minSupport> <startRange:inclusion> <stopRange:exclusion>\nThe number of range can be 0 to 99 (Subject to metadata).", file=sys.stderr)
         exit(-1)
 
-    print("NOTE: The division of \"(stopWindowPosition - startWindowPosition) / windowSize\" must contain no fraction.\nOtherwise, it will be ignore!
-")
+    print("\033[93m" + "NOTE: The division of \"(stopWindowPosition - startWindowPosition) / windowSize\" must contain no fraction. Otherwise, it will be ignore!" + "\033[0m")
     MIN_SUPPORT = int(sys.argv[2])
     START_WINDOW = int(sys.argv[3])
     STOP_WINDOW = int(sys.argv[4])
     WIN_SIZE = int(sys.argv[1])
 
     if STOP_WINDOW < START_WINDOW:
-        print("Invalid wondow range!")
-        quit
+        print("Invalid wondow range!", file=sys.stderr)
+        quit()
 
     windowGroup = []
 
@@ -106,12 +118,14 @@ Efficiency Note: The developer intend to program it easy which is trade off with
     '''
     result = rddWindows.map(frequentSet).collect()
 
-    print(result)
+    outputData = convertFrozenSetToJson(result)
 
-    '''
-    outputFile = "urdmeMiningPartialWindow_" + WIN_SIZE + "_" + MIN_SOPPORT + "_" + STOP_WINDOW + "_" + START_WINDOW + ".json"
-    f = open(outpuFile,'w')
+    print("\033[92m" + outputData + "\033[0m")
+
+    
+    outputFile = "output/urdmeMiningPartialWindow_" + str(WIN_SIZE) + "_" + str(MIN_SUPPORT) + "_" + str(STOP_WINDOW) + "_" + str(START_WINDOW) + ".json"
+    f = open(outputFile,'w')
     f.write(json.dumps(outputData))
     f.close()
-    '''
+    print("Complete!!!")
     sc.stop()
